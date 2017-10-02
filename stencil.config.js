1504ee9eca37b196a14e558a8686b2690d7676af
Tero Saarikivi <saarikivit@gmail.com>
2017-10-02 22:43:45 +0300

    update config

--- stencil.config.js
+++ stencil.config.js
@@ -1,7 +1,16 @@
 exports.config = {
   namespace: 'ttstable',
   generateDistribution: true,
-  bundles: [{ components: ['tts-table'] }]
+  bundles: [
+    {
+      components: [
+        'tts-table',
+        'tts-table-column',
+        'tts-table-item',
+        'tts-side-column'
+      ]
+    }
+  ]
 };
 
 exports.devServer = {
